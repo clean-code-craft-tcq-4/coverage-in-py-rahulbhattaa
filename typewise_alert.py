77ab4f99e1c32294a2bcdac53306d4b03fe4e0b9
rahulbhattaa <73284178+rahulbhattaa@users.noreply.github.com>
2022-07-22 09:59:01 +0530

    Update typewise_alert.py

--- typewise_alert.py
+++ typewise_alert.py
@@ -8,39 +8,39 @@ def infer_breach(value, lowerLimit, upperLimit):
 
 
 def classify_temperature_breach(coolingType, temperatureInC):
-  lowerLimit = 0
-  upperLimit = 0
-  if coolingType == 'PASSIVE_COOLING':
-    lowerLimit = 0
-    upperLimit = 35
-  elif coolingType == 'HI_ACTIVE_COOLING':
-    lowerLimit = 0
-    upperLimit = 45
-  elif coolingType == 'MED_ACTIVE_COOLING':
-    lowerLimit = 0
-    upperLimit = 40
+  coolinglimit={
+    'PASSIVE_COOLING':{'lowerLimit':0,'upperLimit':35},
+    'HI_ACTIVE_COOLING':{'lowerLimit':0,'upperLimit':45},
+    'MED_ACTIVE_COOLING':{'lowerLimit':0,'upperLimit':40}
+  }
+  lowerLimit = coolinglimit[coolingType]['lowerLimit']
+  upperLimit = coolinglimit[coolingType]['upperLimit']
   return infer_breach(temperatureInC, lowerLimit, upperLimit)
 
 
 def check_and_alert(alertTarget, batteryChar, temperatureInC):
-  breachType =\
-    classify_temperature_breach(batteryChar['coolingType'], temperatureInC)
+  breachType =classify_temperature_breach(batteryChar, temperatureInC)
   if alertTarget == 'TO_CONTROLLER':
-    send_to_controller(breachType)
+    alertMessage = send_to_controller(breachType)
   elif alertTarget == 'TO_EMAIL':
-    send_to_email(breachType)
-
+    alertMessage = send_to_email(breachType)
+  else:
+    alertMessage="Not applicable"
+  return alertMessage
 
 def send_to_controller(breachType):
   header = 0xfeed
-  print(f'{header}, {breachType}')
-
+  return(f'{header}, {breachType}')
 
 def send_to_email(breachType):
   recepient = "a.b@c.com"
   if breachType == 'TOO_LOW':
     print(f'To: {recepient}')
     print('Hi, the temperature is too low')
+    return(f'To: {recepient}, Hi, the temperature is too low')
   elif breachType == 'TOO_HIGH':
     print(f'To: {recepient}')
     print('Hi, the temperature is too high')
+    return(f'To: {recepient}, Hi, the temperature is too high')
+  else:
+    return(f'To: {recepient}','Not applicable')
